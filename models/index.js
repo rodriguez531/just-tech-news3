30032e13f5b431d3bb72e11ab2ea23e4cce6ee61
rodriguez531 <alexr5408@gmail.com>
2023-09-12 19:43:44 -0400

    comment routes finished

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require("./User");
 const Post = require("./Post");
 const Vote = require('./Vote');
+const Comment = require('./Comment');
+
 
 //create associations 
 User.hasMany(Post, {
@@ -39,6 +41,22 @@ User.belongsToMany(Post, {
     foreignKey: 'post_id'
   });
 
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
 
 
-  module.exports = { User, Post, Vote };
+
+  module.exports = { User, Post, Vote, Comment };
