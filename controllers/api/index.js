561c42be5750bea0da546101ebb3b12db31c0947
rodriguez531 <alexr5408@gmail.com>
2023-09-25 00:30:50 -0400

    can now login/logout

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,8 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes');
+const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
-
 
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
