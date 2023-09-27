8dbebe5d3ee48dbf004bf104004fc0ecfacaf202
rodriguez531 <alexr5408@gmail.com>
2023-09-27 10:54:34 -0400

    login/logout links accessibilty improved

--- config/connection.js
+++ config/connection.js
@@ -8,7 +8,6 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
       dialect: 'mysql',
-      password: '@flash531Rex',
       port: 3306
     });
 
